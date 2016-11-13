e1e7496e0740da633ea89df0b49b016355ccbe91
mftrhu <mftrhu+github@inventati.org>
2016-11-13 11:18:35 +0100

    Rename files, sketch out engine

--- js/engine.js
+++ js/engine.js
@@ -1,54 +1,16 @@
+/*global $*/
+
 (function (global) {
   "use strict";
-
-  //Returns a random value between min and max, included
-  global.random = function (min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-
-  //Returns a random element from an array
-  Array.prototype.choice = function () {
-    return this[global.random(0, this.length - 1)];
-  };
-
-  //Templates a string according to some simple rules:
-  // - `\`: escape character - add it before one of the following to ignore
-  //   that rule
-  // - `[#(.+?)]`: comment - is ignored
-  // - `[(hidden:)choice 1|choice 2]`: if not hidden, outputs one of choice 
-  //    1 or choice 2, and adds it to the `substitutions` list
-  // - `[@[0-9]+]`: calls one of the earlier substitutions again and outputs 
-  //   it (the substitution must be defined)
-  // - `$[0-9]+`: outputs one of the earlier substitutions
-  String.prototype.parseTemplate = function () {
-    var substitutions = [];
-    return this.replace(/(\\)?(\[(@|#|hidden:)?(.*?)\])/g, function (match, escaped, whole, modifier, choices) {
-      console.log(modifier);
-      if (escaped) {
-        return whole;
-      }
-      if (modifier === "#") {
-        return "";
-      }
-      if (modifier === "@") {
-        choices = substitutions[parseInt(choices, 10)].choices;
-      }
-      var choice = choices.split("|").choice();
-      substitutions.push({
-        choices: choices,
-        choice: choice
-      });
-      if (modifier === "hidden:") {
-        return "";
-      }
-      return choice;
-    }).replace(/(\\)?(\$([0-9]+))/g, function (match, escaped, whole, group) {
-      if (escaped) {
-        return whole;
-      }
-      return substitutions[parseInt(group, 10)].choice;
+  
+  [
+    "subjects"
+  ].forEach(function (element, index, array) {
+    $.getJSON("data/" + element + ".json", function (data, status, xhr) {
+      localStorage["data-" + element] = JSON.stringify(data);
     });
-  };
+    $("#" + element).on("click", function (event) {
+      $("#prompt").html(JSON.parse(localStorage["data-" + element]).choice().parseTemplate());
+    });
+  });
 }(this.window));
