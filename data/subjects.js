0f030cf1eebc8ed95ff9f993f3fb23c239832e19
mftrhu <mftrhu+github@inventati.org>
2016-11-13 10:57:40 +0100

    Add zepto and some subjects, generate a random one

---
+++ data/subjects.js
@@ -0,0 +1,10 @@
+[
+  "Write about something you [won|lost|love|hate].",
+  "Write about someone you [lost|love|hate].",
+  "Write about your [bedroom|kitchen|fridge].",
+  "Write about the moment you were [happiest|saddest].",
+  "Who do you miss the most? What happened to them?",
+  "Have you ever committed a crime? Why?",
+  "Have you ever had hallucinations? What were they about?",
+  "What would you change about your body?"
+]
